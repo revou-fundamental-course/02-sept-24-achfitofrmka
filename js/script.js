5ac283a7fc8de80f41f8d3f5592123936c16e908
fito <achmadfitofarmaka@gmail.com>
2024-09-12 22:08:12 +0700

    js msh ggL

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
 // javascriipt
 
-function kelbtn(){
-    let sisia = document.getElementById("sisi-satu").value;
-    let sisib = document.getElementById("sisi-dua").value;
-    let sisic = document.getElementById("sisi-tiga").value;
-    let hasilkel = parseint(sisia) + parseint(sisib) + parseint(sisic);
-    document.write(`hasilnya adalah ` =hasilkel )
-
-
-    console.log(sisia,sisib,sisic);
-}
-function luasbtn(){
-    let tinggi = document.getElementById("tinggi").value;
-    let panjang = document.getElementById("panjang-alas").value;
+function kelbtn() {
+    let sisia = +document.ElementById("sisi-satu").value;
+    let sisib = +document.getElementById("sisi-dua").value;
+    let sisic = +document.getElementById("sisi-tiga").value;
+    let keliling = parseint(sisia) + parseint(sisib) + parseint(sisic);
+     if (sisia == ``|| sisib == `` || sisic == ``)
+        {alert(`form masih kosong`);}
     
-    let luashasil = parseint(panjang) + parseint(tinggi);
-    document.write(`hasilnya adalah ` =luashasil )
+    
+   }
+     console.log(sisia);
+     console.log(sisib);
+     console.log(sisic);
+    let keliling = sisia + sisib + sisic;
+    document.getElementById('result_keliling').innerHTML = `Keliling = ${sisia} + ${sisib} + ${sisic} = ${keliling}`;
 
+    
+function luasbtn() {
+    let tinggi = +document.getElementById("tinggi").value;
+    let panjang = +document.getElementById("panjang-alas").value;
+    if (panjang == '' || tinggi == '') {
+        alert('Form masih kosong!');
+    
+    let luashasil = (panjang * tinggi)/ 2;
+    document.getElementById('luashasil').innerHTML = `Luas = 1/2 x ${panjang} x ${tinggi} = ${luashasil}`;
+}
 
     console.log(panjang,tinggi);
 }
+
+
+
+prompt
