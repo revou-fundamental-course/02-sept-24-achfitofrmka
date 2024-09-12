9a699a74848c95b5728b2226e1f968161e32fae1
fito <achmadfitofarmaka@gmail.com>
2024-09-12 18:50:18 +0700

    apdet js ...

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-__sccrpt__
+// javascriipt
+
+function kelbtn(){
+    let sisia = document.getElementById("sisi-satu").value;
+    let sisib = document.getElementById("sisi-dua").value;
+    let sisic = document.getElementById("sisi-tiga").value;
+    let hasilkel = parseint(sisia) + parseint(sisib) + parseint(sisic);
+    document.write(`hasilnya adalah ` =hasilkel )
 
 
-pa
+    console.log(sisia,sisib,sisic);
+}
+function luasbtn(){
+    let tinggi = document.getElementById("tinggi").value;
+    let panjang = document.getElementById("panjang-alas").value;
+    
+    let luashasil = parseint(panjang) + parseint(tinggi);
+    document.write(`hasilnya adalah ` =luashasil )
+
+
+    console.log(panjang,tinggi);
+}
